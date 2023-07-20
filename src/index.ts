aa3a4e779437392649df5163380fa6a3e0f244d3
bebossi <bernardobossi77@gmail.com>
2023-07-20 11:38:18 -0300

    ..

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://airbnnbclone.onrender.com"],
+    origin: "https://airbnnbclone.onrender.com",
     credentials: true,
   })
 );
