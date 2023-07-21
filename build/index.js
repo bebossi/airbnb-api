e1d5c1f5f8ef6fe86e46cb6a5156db85d61242cb
bebossi <bernardobossi77@gmail.com>
2023-07-21 04:21:01 -0300

    >.

--- build/index.js
+++ build/index.js
@@ -6,12 +6,32 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 require("dotenv/config");
 var Routes_1 = __importDefault(require("./Routes"));
-var cors_1 = __importDefault(require("cors"));
 var app = (0, express_1.default)();
-app.use((0, cors_1.default)({
-    origin: ["http://localhost:5173", "https://airbnnbclone.onrender.com"],
-    credentials: true,
-}));
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5174",
+//       "https://airbnnbclone.onrender.com",
+//       "https://airbnnbcloneapi.onrender.com",
+//     ],
+//     credentials: true,
+//   })
+// );
+app.use(function (req, res, next) {
+    var allowedOrigins = [
+        "http://localhost:5174",
+        "https://airbnnbclone.onrender.com",
+        "https://airbnnbcloneapi.onrender.com",
+    ];
+    var origin = req.headers.origin;
+    if (origin && allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    next();
+});
 app.use(express_1.default.json());
 app.use(Routes_1.default);
 app.listen(process.env.PORT_EXPRESS, function () {
